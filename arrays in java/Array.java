2aa732e5c246e6aac4b6a9a0f11f9765429d924e
lavanyarayapureddi <lavanyarlvns@gmail.com>
2025-06-20 14:16:04 +0530

    Update Array.java

--- arrays in java/Array.java
+++ arrays in java/Array.java
@@ -192,7 +192,7 @@ for( int i=0;i<b.length;i++)
 System.out.println(b[i]+" ");
 }
 }
-}*/
+}
 
 //copy of one array into another array
 class Arrays
@@ -214,6 +214,28 @@ System.out.print(b[i]+" ");
 }
 }
 
+
+//dupliacte element in an array
+public class Array
+{
+public static void main (String[] args)
+{
+int a[]={1,2,3,4,5};
+for(int i=0;i<a.length;i++)
+{
+    for(int j=i+1;j<a.length;j++)
+    {
+        if(a[i]==a[j])
+        {
+            System.out.println("duplicate element :"+a[i]);
+        }
+        
+        
+    }
+}
+System.out.println("Not found  duplicate element");
+}
+}
 
 
 
